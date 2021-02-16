0af6b8c024863cf69993f25978f9e442f5ff059e
glass-ships <glass.ships@outlook.com>
2021-02-15 21:32:37 -0700

    fuck slides for now

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-console.log('testing js script')
-
 let themeDots = document.getElementsByClassName('browser-dot')
 
 for (var i=0; themeDots.length > i; i++){
   themeDots[i].addEventListener('click',function(){
     console.log('clicked option')
   })
+}
+
+function hover(element, a){
+  b = "images/buttons/" + a;
+  element.setAttribute('src',b);
+}
+
+function unhover(element, a){
+  b = "images/buttons/" + a;
+  element.setAttribute('src', b);
 }
