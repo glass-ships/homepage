77f7ed4e133fb5143605de27513cf68a96ed75a9
glass-ships <glass.ships@outlook.com>
2021-02-22 12:59:11 -0700

    test dummy php backend for heroku

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-let themeDots = document.getElementsByClassName('browser-dot')
-
-for (var i=0; themeDots.length > i; i++){
-  themeDots[i].addEventListener('click',function(){
-    console.log('clicked option')
-  })
-}
-
-function hover(element){
-  // element.style.filter = "hue-rotate(90deg)"  
-  element.setAttribute("style", "width: 7rem; height: 7rem;")
-  element.classList.add("rotate-hue")
-}
-
-function unhover(element){
-  // element.style.filter = "hue-rotate(0deg)"
-  element.setAttribute("style", "width: 5rem; height: 5rem;")
-  element.classList.remove("rotate-hue")
-}
+// Get current year
+var theDate=new Date()
+document.write(theDate.getFullYear())
