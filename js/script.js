4a41a292ef252c41c403bc5ef4f5632c73dc4a10
glass-ships <glass.ships@outlook.com>
2021-04-15 16:50:55 -0600

    format change in js script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // Get current year
-var theDate = new Date();
-document.write(theDate.getFullYear());
+var theDate=new Date()
+document.write(theDate.getFullYear())
 
 // Click to show/hide
 function toggleVis(id) {
-  var e = document.getElementById(id);
-  if (e.style.display == 'block') e.style.display = 'none';
-  else if (e.style.display == 'none') e.style.display = 'block';
-}
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else if(e.style.display == 'none')
+       e.style.display = 'block';
+ }
 
 function toggleProject(value) {
-  a = document.getElementById('project-blurb-wrapper');
-  b = document.getElementById('project-blurb');
-  if (a.style.display == 'none') a.style.display = 'block';
-  b.innerHTML = value;
+   a = document.getElementById('project-blurb-wrapper');
+   b = document.getElementById('project-blurb'); 
+   if(a.style.display == 'none')
+      a.style.display = 'block';
+   b.innerHTML = value;
 }
