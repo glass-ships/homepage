c4c37ca028200cbb86a35781bc7e7fe59aea45fe
glass-ships <glass.ships@outlook.com>
2023-09-01 14:56:44 -0600

    update node modules, remove blur for performance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,86 @@
-import logosvg from '@/assets/images/logo.svg';
-import Image from 'next/image';
+// "use client";
+import Image from "next/image";
+import Logo from "@/assets/images/logo.svg";
 
+// import { useEffect, useState } from "react";
+// // const [isDark, toggleDark] = useDarkMode();
+// const [isShrunk, setShrunk] = useState(false);
+// useEffect(() => {
+//   const onScroll = () => {
+//     setShrunk((isShrunk) => {
+//       if (!isShrunk && (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)) {
+//         return true;
+//       }
+
+//       if (isShrunk && document.body.scrollTop < 4 && document.documentElement.scrollTop < 4) {
+//         return false;
+//       }
+
+//       return isShrunk;
+//     });
+//   };
+
+//   window.addEventListener("scroll", onScroll);
+//   return () => window.removeEventListener("scroll", onScroll);
+// }, []);
 
 export default function Navbar() {
   return (
-<nav 
-  className="
+    <nav
+      className="
     sticky top-0 z-50 flex items-center justify-between 
     w-full px-4 py-2 
     transition-all duration-300 ease-in-out
-   bg-gradient-to-b from-black/10 via-black/50 to-black/30 backdrop-blur-2xl
-">
-  {/* logo */}
-  <a
-  className="flex items-center gap-2 p-8 lg:p-0 scroll-smooth"
-  href="#"
-  rel="noopener noreferrer"
-  >
-    <Image
-      src={logosvg}
-      alt="Logo"
-      className="fill-yellow-400 transition-transform duration-300 ease-in-out transform-gpu hover:scale-110"
-      style={{ 
-        filter: 'drop-shadow(0 0 0.75rem #000)',
-        // fill: '#fff',
-        fill: '#fcd34d',
-        transition: 'fill 0.25s ease-in-out',
-        // transition-timing-function: 'cubic-bezier(0.4, 0, 0.2, 1)',
-      }}
-      width={100}
-      height={100}
-      priority
-    />
-  </a>
+   bg-gradient-to-b from-black/10 via-black/50 to-black/30 
+"
+    >
+      <a className="flex items-center gap-2 p-8 lg:p-0 scroll-smooth" href="#" rel="noopener noreferrer">
+        {/* <Logo /> */}
+        <Image
+          src={Logo}
+          alt="Logo"
+          className="transition-transform duration-300 ease-in-out transform-gpu hover:scale-110"
+          style={{
+            filter: "drop-shadow(0 0 0.75rem #000)",
+            fill: "#fcd34d",
+            transition: "fill 0.25s ease-in-out",
+          }}
+          width={100}
+          height={100}
+          priority
+        />
+      </a>
 
-  <ul className="flex items-center gap-8 font-mono text-sm lg:gap-4 lg:text-base lg:font-thin">
-    {
-      [
-        {
-          label: 'About',
-          href: '#about'
-        },
-        {
-          label: 'Skills',
-          href: '#skills'
-        },
-        {
-          label: 'Projects',
-          href: '#projects'
-        },
-        {
-          label: 'Resume',
-          href: 'https://docs.google.com/document/d/19AHFlI_aqSv938bjgQXM9jodd2I12gmVEhtA8yXsLck/export?format=pdf'
-        },
-        {
-          label: 'Contact',
-          href: '#contact'
-        }
-      ].map((link, index) => (
-        <li key={index} className="px-8">
-          <a
-            className="font-thin text-2xl text-gray-400 hover:text-yellow-300"
-            href={link.href}
-          >
-            {link.label}
-          </a>
-        </li>
-      ))
-    }
-  </ul>
-</nav>
-  )};  
+      <ul className="flex items-center gap-8 font-mono text-sm lg:gap-4 lg:text-base lg:font-thin">
+        {[
+          {
+            label: "About",
+            href: "#about",
+          },
+          {
+            label: "Skills",
+            href: "#skills",
+          },
+          {
+            label: "Projects",
+            href: "#projects",
+          },
+          {
+            label: "Resume",
+            href: "https://docs.google.com/document/d/19AHFlI_aqSv938bjgQXM9jodd2I12gmVEhtA8yXsLck/export?format=pdf",
+          },
+          {
+            label: "Contact",
+            href: "#contact",
+          },
+        ].map((link, index) => (
+          <li key={index} className="px-8">
+            <a className="font-thin text-2xl text-gray-400 hover:text-yellow-300" href={link.href}>
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,34 +1,20 @@
 import Image from "next/image";
-import logo from '@/assets/images/logo-512.png';
-import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
-import { faTerminal } from "@fortawesome/free-solid-svg-icons";
-
+import logo from "@/assets/images/logo-512.png";
 
 export default function Header() {
   return (
-<div
-  // full height
-  className="
+    <div
+      // full height
+      className="
     flex flex-col items-center justify-center
     w-full min-h-screen
   "
->
-    <Image
-      className="relative drop-shadow-[0_0_0.3rem_#ffffff70] w-auto sm:w-52 md:w-80 lg:w-auto h:auto"
-      src={logo}
-      alt="Logo"
-      width={512}
-      height={512}      
-      priority
-    />
+    >
+      <Image className="relative drop-shadow-[0_0_0.3rem_#ffffff70] w-auto sm:w-52 md:w-80 lg:w-auto h:auto" src={logo} alt="Logo" width={512} height={512} priority />
 
-    <h1 className="font-thin text-8xl text-center text-gray-100 pb-4">
-      Glass Ships
-    </h1>
+      <h1 className="font-thin text-8xl text-center text-gray-100 pb-4">Glass Ships</h1>
 
-    <h2 className="font-thin text-5xl text-center text-gray-100 pb-16">
-      Enabling science through software
-    </h2>
-    
-</div>
-)};
+      <h2 className="font-thin text-5xl text-center text-gray-100 pb-16">Enabling science through software</h2>
+    </div>
+  );
+}
