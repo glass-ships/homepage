ab24ec3aebe43217303e374d3337bb7d5a89f4ee
glass-ships <glass.ships@outlook.com>
2024-04-01 12:51:17 -0600

    try adding favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,14 +10,26 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Glass Ships",
   description: "Scientific Software Engineer",
+  // icons: {
+  //   icon: [
+  //     {
+  //       rel: "icon",
+  //       type: "image/png",
+  //       url: "/logo.png",
+  //     },
+  //   ],
+  // },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
+          <link rel="shortcut icon" href="/images/logo.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png"/>
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" /> */}
+</Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
