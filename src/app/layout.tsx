83db1ad43d7161960dff9f443188208100e45753
glass-ships <glass.ships@outlook.com>
2024-04-01 13:19:40 -0600

    fix a few issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,26 +10,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Glass Ships",
   description: "Scientific Software Engineer",
-  // icons: {
-  //   icon: [
-  //     {
-  //       rel: "icon",
-  //       type: "image/png",
-  //       url: "/logo.png",
-  //     },
-  //   ],
-  // },
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        // type: "image/png",
+        url: "/images/favicon.ico",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-          <link rel="shortcut icon" href="/images/logo.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png"/>
-          {/* <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" /> */}
-</Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
