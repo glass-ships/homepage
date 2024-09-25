53b75733f8ade072e5d60f86c41845a932dbf475
glass-ships <glass.ships@outlook.com>
2024-09-25 13:49:58 -0400

    negligible tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-// import { config } from "@fortawesome/fontawesome-svg-core";
-// import "@fortawesome/fontawesome-svg-core/styles.css";
-// config.autoAddCss = false;
-
 import Navbar from "@/components/Navbar";
 import Header from "@/pages/Header";
 import About from "@/pages/About";
@@ -12,7 +8,6 @@ import Projects from "@/pages/Projects";
 import Contact from "@/pages/Contact";
 import Footer from "@/components/Footer";
 import "@/global/styles.scss";
-// import AppTestIframe from "@/components/AppTestIframe";
 
 export default function Home() {
   return (
